4ad797f5eb829d5485f61125c2770a3d8df0917a
Your Name <your-email@example.com>
2025-12-27 23:48:47 +0530

    intislising vite-react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-green-400">
+        Tailwind Working 🔥
+      </h1>
+    </div>
+  );
+}
+
+export default App
